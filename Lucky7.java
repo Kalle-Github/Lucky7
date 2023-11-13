34657ff3b911207bd2ea42f9f249f1b9a0997511
Kalle-Github <kalle23011@student.hamk.fi>
2023-11-13 19:30:17 +0200

    Voittojen tarkistus

--- Lucky7.java
+++ Lucky7.java
@@ -9,6 +9,7 @@ public class Lucky7 {
         int luku, luku2, luku3;
         int raha = 5;
         String vastaus;
+        int seiskat;
 
         do {
             System.out.println("Sinulla on " + raha + " € Peli maksaa 1€");
@@ -28,6 +29,20 @@ public class Lucky7 {
                 if (raha == 0) {
                     System.out.println("Rahat loppuivat");
                 }
+
+                seiskat = laskeSeiskat(luku, luku2, luku3);
+if (seiskat == 1) {
+                    raha += 3;
+                    System.out.println("Sait yhden seiskan, voitat 3€");
+                } else if (seiskat == 2) {
+                    raha += 5;
+                    System.out.println("Sait kaksi seiskaa, voitat 5€");
+                } else if (seiskat == 3) {
+                    raha += 10;
+                    System.out.println("Sait kolme seiskaa, voitat 10€");
+                } else {
+                    System.out.println("Ei voittoa tällä kertaa");
+                }
             } else if (vastaus.equals("e")) {
                 break;
             } else {
@@ -37,5 +52,19 @@ public class Lucky7 {
             System.out.println("Haluatko pelata uudestaan? Paina enter pelataksesi tai e peruuttaaksesi");
 
         } while (raha > 0);
+    }
+
+    private static int laskeSeiskat(int luku1, int luku2, int luku3) {
+        int seiskat = 0;
+        if (luku1 == 7) {
+            seiskat++;
+        }
+        if (luku2 == 7) {
+            seiskat++;
+        }
+        if (luku3 == 7) {
+            seiskat++;
+        }
+        return seiskat;
     }
 }
